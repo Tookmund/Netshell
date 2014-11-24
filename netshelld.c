d0f022e0168d8b382221af98cda8f30e68b51c3b
Jacob Adams <tookmund@gmail.com>
2014-11-24 18:28:41 -0500

    Always print errors

--- netshelld.c
+++ netshelld.c
@@ -1,7 +1,5 @@
 /*
  * A simple Telnet clone designed for easy remote access
- * 
- *
  * By Jacob Adams <tookmund@gmail.com>
  */
 
@@ -17,13 +15,11 @@
 #include <arpa/inet.h> 	/* For inet_addr() */
 #include <unistd.h> 	/* For close() */
 #include <netinet/in.h> /* For sockaddr_in */
-#include <limits.h>
 
-#define DEBUG 		/* Enable to print debug messages, can be enabled at 
+#define DEBUG 		/* Enable to print debug messages, can be enabled at
 			 * compile time via -DDEBUG
 			 */
 #define ADDR 12 	/* Length of an IPv4 address plus null */
-#define MAX 1024 	/* Largest message that can be recived */
 #define SHELL "/bin/sh" /* Program to use as shell */
 #define PORT "8888"	/* The port netshelld will listen on */
 
@@ -35,42 +31,43 @@
 void child(int sfd) {
 	dup2(sfd,0);
 	dup2(sfd,1);
+	dup2(sfd,2);
 	char* args[] = {SHELL};
 	execv(SHELL,args);
 }
 
 int main (void) {
-	/*
-	struct protoent *pro;
-	pro = getprotobyname("tcp");
-	int protocol;
-	if (pro == NULL) protocol = 0;
-	else protocol = pro->p_proto;
-	/* Create an socket to listen on 
-	int srv;
-	/* int srv = socket(AF_INET,SOCK_STREAM,protocol); 
-	if (srv == -1) perror("Socket");
-	*/
 	int srv;
 	struct addrinfo hints;
-	struct addrinfo addr;
+	struct addrinfo *addr;
 	memset(&hints,0,sizeof(hints));
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_ADDRCONFIG;
-	struct addrinfo *ptr = &addr;
-	int gai = getaddrinfo("localhost",PORT,&hints,&ptr);
-	if (gai != 0) DBG(gai_strerror(gai));
+	hints.ai_flags = AI_ADDRCONFIG | AI_PASSIVE;
+	int gai = getaddrinfo(NULL,PORT,&hints,&addr);
+	if (gai != 0) {
+		printf("getaddrinfo error\n");
+		printf(gai_strerror(gai));
+	}
 	/* borrow socket check from libsocket */
 	struct addrinfo *check;
-	for (check = ptr; check != NULL; check = check->ai_next) {
+	for (check = addr; check != NULL; check = check->ai_next) {
+		printf("Trying %s\n",check->ai_addr->sa_data);
 		srv = socket(check->ai_family,check->ai_socktype,check->ai_protocol);
 		if (srv < 0) continue;
-		if (-1 != connect(srv,check->ai_addr,check->ai_addrlen)) break;
+		if (-1 != bind(srv,check->ai_addr,check->ai_addrlen)) {
+			printf("Listening on %s\n",check->ai_addr->sa_data);
+			const int       optVal = 1;
+			const socklen_t optLen = sizeof(optVal);
+			int rtn = setsockopt(srv, SOL_SOCKET, SO_REUSEADDR, (void*) &optVal, optLen);
+			if (rtn != 0) perror("setsockopt on srv");
+			break;
+		}
 	}
+	freeaddrinfo(addr);
 	listen(srv,10);
 	for (;;) {
-		DBG("\nWaiting...\n");
+		DBG("Waiting...\n");
 		int sfd = accept(srv,NULL,NULL);
 		DBG("\nForking\n");
 		int pid = fork();
@@ -79,6 +76,7 @@ int main (void) {
 		}
 		else if (pid == 0) {
 			child(sfd);
+			close(sfd);
 			exit(0);
 		}
 		else {
